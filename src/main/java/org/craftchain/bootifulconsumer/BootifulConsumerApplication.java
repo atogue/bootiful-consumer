cf28c92ae853725c1f4ced178c5fe85ee6df459a
Anicet Togue <atogue@gmail.com>
2021-01-04 20:50:11 +0100

    autowired webClient to handle http verbs

--- src/main/java/org/craftchain/bootifulconsumer/BootifulConsumerApplication.java
+++ src/main/java/org/craftchain/bootifulconsumer/BootifulConsumerApplication.java
@@ -2,9 +2,16 @@ package org.craftchain.bootifulconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class BootifulConsumerApplication {
+
+	@Bean
+	WebClient client(WebClient.Builder builder) {
+		return builder.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootifulConsumerApplication.class, args);
