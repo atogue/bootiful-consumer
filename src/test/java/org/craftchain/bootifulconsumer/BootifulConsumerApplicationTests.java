febbf2b752dafe05d9a42c7072ac8be6bd2382c5
Anicet Togue <atogue@gmail.com>
2021-01-04 19:41:25 +0100

    testing consumer contextLoads

---
+++ src/test/java/org/craftchain/bootifulconsumer/BootifulConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.craftchain.bootifulconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
